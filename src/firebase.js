d417f1655ac7a3508bf2eef5d1c4ecd3bdad5e59
Addr678 <im_yadavprashantkumar33@gmail.com>
2023-09-29 10:36:42 +0530

    add

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,23 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore, doc, setDoc } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyATGfDWEySyw6yw5jLnajlGoHv2m2nckNA",
-  authDomain: "podcast-react-app-4da1e.firebaseapp.com",
-  projectId: "podcast-react-app-4da1e",
-  storageBucket: "podcast-react-app-4da1e.appspot.com",
-  messagingSenderId: "236964049974",
-  appId: "1:236964049974:web:ac0ddd840294796c1ed497",
-  measurementId: "G-JJFNBP7BH0"
+  apiKey: "AIzaSyADAlolLMT4s_bS7Zpa3KaTwzpsZZsyMY4",
+  authDomain: "personalfinancetrackerreactapp.firebaseapp.com",
+  projectId: "personalfinancetrackerreactapp",
+  storageBucket: "personalfinancetrackerreactapp.appspot.com",
+  messagingSenderId: "936808586103",
+  appId: "1:936808586103:web:4d844945d1941d3cae5f72",
+  measurementId: "G-J10BLX4606"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
